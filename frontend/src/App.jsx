588f6a701ef19493ea191b5233c4f442277407be
yairyairyair <24393372+yairyairyair@users.noreply.github.com>
2023-08-30 17:23:42 +0300

    remove unneeded css margin code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,12 +45,6 @@ const App = () => {
               width={500}
               height={300}
               data={recalls}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
             >
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="date" tickFormatter={formatDate} />
